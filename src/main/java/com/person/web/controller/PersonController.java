e0bc01cb5e30bf97ef420e35b797a36d65d3874a
Vlad Wild <vladwild220@gmail.com>
2022-05-28 19:45:54 +0300

    3) Добавлен slf4j для логирования

--- src/main/java/com/person/web/controller/PersonController.java
+++ src/main/java/com/person/web/controller/PersonController.java
@@ -1,6 +1,8 @@
 package com.person.web.controller;
 
 import com.person.web.dto.PersonDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,10 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
         produces = MediaType.APPLICATION_JSON_VALUE
 )
 public class PersonController {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public Long savePerson(@RequestBody PersonDto dto) {
-        System.out.println(dto);
+        logger.info("PersonDto: {}", dto);
         return null;
     }
 }
